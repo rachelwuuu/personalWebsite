6ea82ad3f2a95c54b2d44922bb75a7e82457c976
Rachel Wu <ruoxuan.wu@mail.utoronto.ca>
2021-05-20 22:46:57 -0400

    added min width to navbar

--- Documents/tech projects/Website Project/personal portafolio/personalWebsite/personalWebsite/src/app/app.component.ts
+++ Documents/tech projects/Website Project/personal portafolio/personalWebsite/personalWebsite/src/app/app.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Observable, pipe} from 'rxjs';
 import {fromEvent} from 'rxjs';
-fromEvent(document.body, 'mousemove').subscribe(e=>{console.log(e);})
+fromEvent(document.body, 'mousemove').subscribe(($e)=>{
+  
+  //const target=Object.assign(e.lngLat);
+  //const xpos=target.bubbles;
+  //console.log($e.clientX);
+})
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
